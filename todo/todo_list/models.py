0dc18c77ffa2955009baa46cc9682ccd7f47eeec
SandipanDas <50514440+sandipan898@users.noreply.github.com>
2020-05-11 09:49:19 -0700

    update models.py

--- todo/todo_list/models.py
+++ todo/todo_list/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class List(models.Model):
+    user = models.ForeignKey(User, related_name="list_user", on_delete=models.CASCADE, null=True)
     item = models.CharField(max_length = 300)
     completed = models.BooleanField(default=False)
-    time_added = models.DateTimeField(auto_now=True)
-
+    time_added = models.DateTimeField(auto_now_add=True) 
+    
     def __str__(self):
         return f"{self.item} {self.completed}"
