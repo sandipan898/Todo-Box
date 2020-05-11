fdeb8e3aa8c61e0ec974f32a4cf1818071d505b5
SandipanDas <50514440+sandipan898@users.noreply.github.com>
2020-05-11 09:40:45 -0700

    changing url for signup view

--- todo/app_user/urls.py
+++ todo/app_user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import SignUpView, user_home_view
+from .views import signup_form, user_home_view, MySignUpView
 from django.contrib.auth.views import LoginView, LogoutView
+from allauth.account.views import SignupView
+
 
 urlpatterns = [
     path('home/', user_home_view, name='user_home'),
     path('login/', LoginView.as_view(template_name="app_user/login_form.html"), name='user_login'),
     path('logout/', LogoutView.as_view(), name='user_logout'),
-    path('signup/', SignUpView.as_view(), name='user_signup'),
+    #path('signup/', MySignUpView.as_view(), name='user_signup'),
+    path('signup/', signup_form, name='user_signup'),
+    #path('signup/', SignupView.as_view(), name='user_signup'),
 ]
