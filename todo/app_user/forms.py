ba37f253faa7c679f85c702e750ed84b44df7050
SandipanDas <50514440+sandipan898@users.noreply.github.com>
2020-05-11 08:39:36 -0700

    Update forms.py

--- todo/app_user/forms.py
+++ todo/app_user/forms.py
@@ -1,19 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from allauth.account.forms import SignupForm
 
-
-class SignUpForm(UserCreationForm):
-    
-    email = forms.EmailField(label = "Email", )
-    first_name = forms.CharField(label = "First name")
-    last_name = forms.CharField(label = "Last name")
-
-    class Meta:
-        model = User
-        fields = ("username", "first_name", "last_name", "email", )
-
-
+"""
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
@@ -22,3 +12,30 @@ class SignUpForm(UserCreationForm):
         if commit:
             user.save()
         return user
+        """
+
+class MySignUpForm(UserCreationForm):
+    email = forms.EmailField(label = "Email",)
+    first_name = forms.CharField(label = "First name")
+    last_name = forms.CharField(label = "Last name")
+    username = forms.TextInput(),
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+        """
+        widgets = {
+            "first_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "last_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "username": forms.TextInput(attrs={'class': 'form-control'}),
+            #"email": forms.EmailField(attrs={'class': 'from-control'}),
+            "password1": forms.PasswordInput(attrs={'class': 'form-control'}),
+            "password2": forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+        """
