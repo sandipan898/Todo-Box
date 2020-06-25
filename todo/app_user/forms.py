548e116d27c6052da1504ad43a9db6fd5413a327
sandipan898 <sandipan.das898@gmail.com>
2020-06-25 11:23:19 -0700

    Update signup form

--- todo/app_user/forms.py
+++ todo/app_user/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django import forms
 from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth import models
+from django.contrib.auth.password_validation import password_validators_help_text_html
 # from allauth.account.forms import SignupForm
 # from django.core import validators
 # from django.contrib.auth.password_validation import UserAttributeSimilarityValidator, MinimumLengthValidator
@@ -25,27 +26,36 @@ from allauth.account.forms import SignupForm
 class MySignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={
                     'class': 'form-control',
+                    'style': 'width: 500px',
         }))
     first_name = forms.CharField(label="First name", max_length=100, 
                 widget=forms.TextInput(attrs={
                     'class': 'form-control',
+                    'style': 'width: 500px',
         })
     )
     last_name = forms.CharField(label="Last name", max_length=100, widget=forms.TextInput(attrs={
                     'class': 'form-control',
+                    'style': 'width: 500px',
         }))
     
     user_name = UsernameField(widget=forms.TextInput(attrs={
                     'class': 'form-control',
+                    'style': 'width: 500px',
         }))
-    password1 = forms.PasswordInput(
-                    #widget=forms.PasswordInput(attrs={
-                    #'class': 'form-control',
-        )
-    password2 = forms.PasswordInput(
-                    #widget=forms.PasswordInput(attrs={
-                    #'class': 'form-control',
-        )
+    password1 = forms.CharField(
+                    widget=forms.PasswordInput(attrs={
+                    'class': 'form-control',
+                    'style': 'width: 500px',
+                    'autocomplete': 'new-password',
+                    }), help_text=password_validators_help_text_html())
+    password2 = forms.CharField(
+                    widget=forms.PasswordInput(attrs={
+                    'class': 'form-control',
+                    'style': 'width: 500px',
+                    'autocomplete': 'new-password',
+                    }), help_text=("Enter the same password as before, for verification."))
+
     class Meta:
         model = User
         fields = [
