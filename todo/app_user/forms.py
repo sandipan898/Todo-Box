97a8b9d8758ab8ccd9c3a2521b0022f53d636b51
SandipanDas <50514440+sandipan898@users.noreply.github.com>
2020-06-25 08:56:47 -0700

    Update forms.py

--- todo/app_user/forms.py
+++ todo/app_user/forms.py
@@ -1,19 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth import models
+from allauth.account.forms import SignupForm
+from django.core import validators
+from django.contrib.auth.password_validation import UserAttributeSimilarityValidator, MinimumLengthValidator
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class MySignUpForm(UserCreationForm):
-    email = forms.EmailField(label="Email",)
+    email = forms.EmailField(label="Email")
     first_name = forms.CharField(label="First name")
     last_name = forms.CharField(label="Last name")
-    user_name = forms.CharField(label="username")
+    
+    user_name = UsernameField()
 
     class Meta:
         model = User
         fields = [
             "first_name",
             "last_name",
-            "user_name"
+            "user_name",
             "email",
         ]
+    
+        def clean_user_name(self):
+            username = self.cleaned_data['username']
+            if len(username) < 2:
+                raise forms.ValidationError("Username is too short")
+            elif User.objects.get(username=username):
+                raise forms.ValidationError("This username is already taken")
+            return username
