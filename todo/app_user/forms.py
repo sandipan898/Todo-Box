afd0b0966159d630abf43b809ac4051e574c2940
sandipan898 <sandipan.das898@gmail.com>
2020-06-27 08:37:59 -0700

    Update signup form

--- todo/app_user/forms.py
+++ todo/app_user/forms.py
@@ -26,33 +26,33 @@ from allauth.account.forms import SignupForm
 class MySignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
         }))
     first_name = forms.CharField(label="First name", max_length=100, 
                 widget=forms.TextInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
         })
     )
     last_name = forms.CharField(label="Last name", max_length=100, widget=forms.TextInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
         }))
     
     user_name = UsernameField(widget=forms.TextInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
         }))
     password1 = forms.CharField(
                     widget=forms.PasswordInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
                     'autocomplete': 'new-password',
                     }), help_text=password_validators_help_text_html())
     password2 = forms.CharField(
                     widget=forms.PasswordInput(attrs={
                     'class': 'form-control',
-                    'style': 'width: 500px',
+                    #'style': 'width: 500px',
                     'autocomplete': 'new-password',
                     }), help_text=("Enter the same password as before, for verification."))
 
